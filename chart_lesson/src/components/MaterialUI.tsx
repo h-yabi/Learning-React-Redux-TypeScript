0c8aecfb68138590fc5f27f8d1f8e55ffa2603b1
h_yabiku <>
2022-04-17 14:50:00 +0900

    MaterialUI - Grid を使ってみた

--- chart_lesson/src/components/MaterialUI.tsx
+++ chart_lesson/src/components/MaterialUI.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, makeStyles, Typography } from '@material-ui/core';
+import { Button, makeStyles, Typography, Grid, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles({
   buttonStyle: {
@@ -9,6 +9,10 @@ const useStyles = makeStyles({
   typeStyle: {
     color: 'skyblue',
     padding: '3px 50px',
+  },
+  paperStyle: {
+    background: 'orange',
+    height: '50px',
   },
 });
 
@@ -26,6 +30,19 @@ const MaterialUI: React.FC = () => {
       >
         テストテストテストテストテストテスト
       </Typography>
+
+      <Grid container spacing={1}>
+        <Grid item sm={12}>
+          <Paper className={classes.paperStyle}>xs=12</Paper>
+        </Grid>
+        <Grid item sm={6}>
+          <Paper className={classes.paperStyle}>xs=6</Paper>
+        </Grid>
+        <Grid item sm={6}>
+          <Paper className={classes.paperStyle}>xs=6</Paper>
+        </Grid>
+      </Grid>
+
       <Button
         className={classes.buttonStyle}
         variant="contained"
