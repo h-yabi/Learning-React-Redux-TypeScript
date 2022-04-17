bec88129fe7874e7ce1c21002bb28ca8a8ff4ebd
h_yabiku <>
2022-04-17 13:55:56 +0900

    Typography を使ってみた

--- chart_lesson/src/components/MaterialUI.tsx
+++ chart_lesson/src/components/MaterialUI.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, makeStyles } from '@material-ui/core';
+import { Button, makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   buttonStyle: {
     background: 'green',
+    padding: '3px 50px',
+  },
+  typeStyle: {
+    color: 'skyblue',
     padding: '3px 50px',
   },
 });
@@ -12,6 +16,16 @@ const MaterialUI: React.FC = () => {
   const classes = useStyles();
   return (
     <div>
+      <Typography
+        className={classes.typeStyle}
+        color="secondary"
+        variant="h1"
+        align="left"
+        gutterBottom
+        noWrap
+      >
+        テストテストテストテストテストテスト
+      </Typography>
       <Button
         className={classes.buttonStyle}
         variant="contained"
