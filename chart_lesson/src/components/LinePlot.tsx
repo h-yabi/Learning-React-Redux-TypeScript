1aff118843ebacd52dd04b9a0c736df135a5cac5
h_yabiku <>
2022-04-17 11:17:15 +0900

    簡単な chart を表示

--- chart_lesson/src/components/LinePlot.tsx
+++ chart_lesson/src/components/LinePlot.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Chart, registerables } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-const LinePlot = () => {
-  return <div></div>;
+Chart.register(...registerables);
+
+const data = {
+  labels: ['月', '火', '水', '木', '金', '土', '日'],
+  datasets: [
+    {
+      label: 'Demo line plot',
+      backgroundColor: '#008080',
+      borderColor: '#7fffd4',
+      pointBorderWidth: 10,
+      data: [5, 6, 9, 15, 30, 40, 80],
+    },
+  ],
+};
+
+const LinePlot: React.FC = () => {
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  );
 };
 
 export default LinePlot;
