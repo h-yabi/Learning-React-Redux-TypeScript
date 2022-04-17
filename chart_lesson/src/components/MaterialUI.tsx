fcd1e1726f8e200e69b1116d2dc7f3d1222741d2
h_yabiku <>
2022-04-17 13:48:05 +0900

    MaterialUI の style を変更してみた

--- chart_lesson/src/components/MaterialUI.tsx
+++ chart_lesson/src/components/MaterialUI.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  buttonStyle: {
+    background: 'green',
+    padding: '3px 50px',
+  },
+});
 
 const MaterialUI: React.FC = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Button variant="contained" color="primary">
+      <Button
+        className={classes.buttonStyle}
+        variant="contained"
+        color="primary"
+      >
         Test Button
       </Button>
     </div>
